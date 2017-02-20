d27f6494d13886b566ca1ad47886990e86fb7609
tangtao <rache008@163.com>
2017-02-20 16:17:32 +0800

    第一次提交

---
+++ douban/main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+#name   = main
+#author = tangtao
+#time   = 2017/2/20 16:04
+#Description=添加描述信息
+#eMail   =tangtao@lhtangtao.com
+#git     =lhtangtao
+# code is far away from bugs with the god animal protecting
+I love animals. They taste delicious.
+┏┓      ┏┓
+┏┛┻━━━┛┻┓
+┃      ☃      ┃
+┃  ┳┛  ┗┳  ┃
+┃      ┻      ┃
+┗━┓      ┏━┛
+┃      ┗━━━┓
+┃  神兽保佑    ┣┓
+┃　永无BUG！   ┏┛
+┗┓┓┏━┳┓┏┛
+┃┫┫  ┃┫┫
+┗┻┛  ┗┻┛
+"""
+import sys
+from scrapy import cmdline
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+cmdline.execute("scrapy crawl douban".split())
